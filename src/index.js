7f2431e8e15231c38b2721d54d498a1f90ed5118
hollow <placeholder>
2026-04-23 08:39:21 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+console.log(path.resolve(__dirname, "../", "public"))
+app.use('/public', express.static(path.resolve(__dirname, "css")))
+app.use('/public', express.static(path.resolve(__dirname, "../", "public")))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+

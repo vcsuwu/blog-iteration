00896b955a9507767ef1c60214088f23de98410a
hollow <placeholder>
2026-04-23 11:33:18 +0500

    [CHANGE]: Now i really made the thing the way it should be :)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/public', express.static(path.resolve(__dirname, "../", "public")))
+app.use('/public', express.static(path.resolve(__dirname, "public")))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
